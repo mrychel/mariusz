be7765c22bc9f9934e30abeb4a53bd3cb6c0181f
Miroslaw Rychel <mrychel@guidewire.com>
2018-11-20 10:12:49 +0100

    Trojkaty i pola.

--- zabawki/src/zabawki/Dziedziczenie.java
+++ zabawki/src/zabawki/Dziedziczenie.java
@@ -3,8 +3,17 @@ package zabawki;
 public class Dziedziczenie {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Trojkat pierwsza_instancja_trojkata = new Trojkat(10, 3);
+		Trojkat inna_instancja_trojkata = new Trojkat(5, 4);
+		
+		double pole_jakiegos_jeszcze_innego_trojkata = (new Trojkat(6, 9)).pole();
+		
+		System.out.println("pole pierwszego trojkata: "+pierwsza_instancja_trojkata.pole());
+		System.out.println("pole innej instancji trojkata: "+inna_instancja_trojkata.pole());
+		System.out.println("pole_jakiegos_jeszcze_innego_trojkata: "+pole_jakiegos_jeszcze_innego_trojkata);
 	}
+	
+	
 
 }
---
+++ zabawki/src/zabawki/Trojkat.java
@@ -0,0 +1,18 @@
+package zabawki;
+
+
+
+public class Trojkat {
+
+	
+	int _a, _b, _c, _h;
+	
+	Trojkat(int a, int h ) {
+		_a = a;
+		_h = h;
+	}
+	
+	double pole () {
+		return _a * _h * 0.5;
+	} 
+}
